3411525c6735f84ba32e8fb64ec8a657bf0cdbea
onited <baudrydorian@yahoo.fr>
2023-11-04 21:18:43 +0100

    Update dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/My_Portfolio/'
+    : '/',
+  transpileDependencies: true,
+});
